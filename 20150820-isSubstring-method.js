cd4c6265bc6a95211e9301e048c739604b321d9c
Anna Casper <anna.casper3@gmail.com>
2015-08-20 10:16:50 -0600

    compressed string complete

--- 20150820-isSubstring-method.js
+++ 20150820-isSubstring-method.js
@@ -4,18 +4,41 @@ var str1 = 'helloworld'
 var str2 = 'worldhello'
 var str3 = 'notthesamestring'
 
-var new1 = str1.split('').sort().join('')
-var new2 = str2.split('').sort().join('')
-var new3 = str3.split('').sort().join('')
-
-var isSubstring = function (str1, str2) {
-  if(str1.indexOf(str2) >= 0 || str2.indexOf(str1) >= 0){
-    return true
+var stringSort = function (str1, str2) {
+  var new1 = str1.split('').sort().join('')
+  var new2 = str2.split('').sort().join('')
+  if(new1.length > new2.length){
+    return new1.indexOf(new2)
   } else {
-    return false
+    return new2.indexOf(new1)
   }
-
 }
 
-console.log(isSubstring(new1, new2));
-console.log(isSubstring(new1, new3));
+// console.log(stringSort(str1, str2));
+// console.log(stringSort(str1, str3));
+
+//Implement a method to perform basic string compression using the counts of repeated characters.
+var string = 'aaaabbbbccdddeeeeeffGHAAA'
+var compressString = function (string) {
+  var array = string.split('');
+  var accum = 1;
+  var letter = ''
+  var result = ''
+  array.forEach(function (char, i, array) {
+    if(letter === char){
+      accum += 1
+    } else {
+      result += letter + accum
+      letter = char
+      accum = 1;
+    }
+  })
+  result = result.slice(1, result.length-1)
+  if (result.length > string.length) {
+    return string
+  } else {
+    return result
+  }
+}
+
+console.log(compressString(string))

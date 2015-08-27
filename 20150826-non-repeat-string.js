85619312fbab9b9d2a4f057caec124afdb544552
Anna Casper <anna.casper3@gmail.com>
2015-08-27 09:10:39 -0600

    working on recursion

--- 20150826-non-repeat-string.js
+++ 20150826-non-repeat-string.js
@@ -4,9 +4,10 @@ var str2 = 'one afternoon';
 var noRepeat = function (str) {
   var arr = str.split('');
   var result = '';
-  var newArray;
-  for (var i = 0; i < arr.length; i++) { 
+  var newArray;a
+  for (var i = 0; i < arr.length; i++) {
     newArr = arr;
+    console.log(newArr);
     newArr.splice(i, 1)
     console.log(newArr);
     console.log(newArr.indexOf(arr[i]));

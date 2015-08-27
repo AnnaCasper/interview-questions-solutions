a49bd3e21065139defd07164c91b437db9b956dd
Anna Casper <anna.casper3@gmail.com>
2015-08-27 10:07:34 -0600

    recursion complete

--- 20150827-recursion.js
+++ 20150827-recursion.js
@@ -2,8 +2,39 @@ var power = function (base, exponent) {
   if(exponent === 0){
     return 1
   } else {
-    return base * poser(base, exponent-1)
+    return base * power(base, exponent-1)
   }
 }
 
-console.log(power(2, 3));
+
+var reverse = function (s) {
+  if(s.length == 0){
+    return s;
+  } else {
+    console.log(s);
+    console.log(s.substring(1));
+    console.log(s.charAt(0));
+    return reverse(s.substring(1)) + s.charAt(0)
+  }
+}
+
+thing = reverse('apple');
+console.log(thing);
+
+
+// ""
+// (reverse('') + 'e'
+// (reverse('e') + 'l'
+// (reverse('le') + 'p'
+// (reverse('ple') + 'p'
+// (reverse('pple') + 'a'
+//
+//
+//
+// ""
+// ("" + 'e')
+// ("e" + 'l')
+// ("el" + 'p')
+// ("elp" + 'p')
+// ("elpp" + 'a'
+// "elppa"

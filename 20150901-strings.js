ce334f40e7c9d28140b8d0b80a7b74658837aff5
Anna Casper <anna.casper3@gmail.com>
2015-09-01 09:47:52 -0600

    string search complete

---
+++ 20150901-strings.js
@@ -0,0 +1,49 @@
+var string= "Mississippi";
+sub = string.indexOf('iss');
+//console.log(sub);
+
+var search = function (str) {
+  var array = ['Anna', 'Emily', 'Andrea', 'Drew', 'Laura'];
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    if(array[i].toLowerCase().indexOf(str) >= 0){
+      result.push(array[i])
+    }
+  }
+  return result;
+}
+
+//console.log(search('dre'));
+
+var fuzzy = function (str) {
+  var array = ['Anna', 'Clarkson', 'Andrea', 'Drew', 'Laura'];
+  var newArray = str.split('')
+  var results = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < newArray.length; j++) {
+    var result = 0;
+      if(array[i].toLowerCase().indexOf(newArray[j].toLowerCase()) >= 0){
+        result += array[i].toLowerCase().indexOf(newArray[j])
+      }
+    }
+    if(result >= 3){
+      results.push(array[i])
+    }
+  }
+  return results;
+}
+
+//console.log(fuzzy('Clerk'));
+var result = 0;
+
+var appearing = function (compare, str) {
+  if(compare.indexOf(str) < 0){
+    return result;
+  } else if(compare.indexOf(str) >= 0){
+    var sub = compare.substring(compare.indexOf(str) + str.length, compare.length)
+    result += 1
+    return appearing(sub, str)
+  }
+}
+
+console.log(appearing('Mississippi', 'iss'));
